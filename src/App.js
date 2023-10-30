a85518fa7cd94c290577422c4d2890c14b2ad1b7
Akash <akash.mistry@innodeed.com>
2023-10-31 02:08:40 +0530

    Making everything stateful

--- src/App.js
+++ src/App.js
@@ -1,94 +1,72 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Button from "./components/Button";
 import Tooltip from "./components/Tooltip";
 
-export default class App extends Component {
-  // CONSTRUCTOR TO CREATE THE STATE OF THE CLASS
-  constructor() {
-    super();
-    this.state = {
-      direction: "top",
-      content: "Tooltip Content",
-    };
-  }
+const App = () => {
+  const [direction, setDirection] = useState("top");
+  const [content, setContent] = useState("");
 
   // TEXT INPUT HANDLER
-  handleContentInput = (e) => {
-    console.log(e.target.value);
-    if (e.target.value.length > 0) {
-      this.setState({
-        content: e.target.value,
-      });
-    } else {
-      this.setState({
-        content: "Tooltip Content",
-      });
-    }
+  const handleContentInput = (e) => {
+    setContent(e.target.value);
   };
 
   // SET THE DIRECTION OF THE TOOLTIP
-  handleDirectionInput = (e) => {
-    console.log(e.target.value);
-    this.setState({
-      direction: e.target.value,
-    });
+  const handleDirectionInput = (e) => {
+    setDirection(e.target.value);
   };
-  render() {
-    return (
+  return (
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "200px",
+      }}
+    >
       <div
-        className="App"
         style={{
           display: "flex",
-          justifyContent: "center",
-          marginTop: "200px",
+          flexDirection: "column",
+          width: "400px",
+          height: "500px",
+          backgroundColor: "beige",
+          borderRadius: "10px",
         }}
       >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            width: "400px",
-            height: "500px",
-            backgroundColor: "beige",
-            borderRadius: "10px",
-          }}
-        >
-          {/* HEADING */}
-          <h1 style={{ color: "gray", fontWeight: "bolder" }}>Tooltip</h1>
-          <div className="form-div">
-            <label style={{ color: "gray" }}>Enter Content of Tooltip</label>
-            {/* CHANGE THE TEXT OF THE TOOLTIP TEXT */}
-            <input
-              style={{ borderRadius: "10px" }}
-              type="text"
-              className="form-control"
-              onKeyUp={this.handleContentInput}
-            />
-          </div>
-          <div className="form-div">
-            <label style={{ color: "gray" }}>Enter Direction of Tooltip</label>
-            {/* TO SELECT THE DIRECTION OF THE TOOLTIP */}
-            <select
-              className="form-select"
-              onChange={this.handleDirectionInput}
-              style={{ borderRadius: "10px", padding: "5px 10px" }}
-            >
-              <option value="top">Top</option>
-              <option value="left">Left</option>
-              <option value="right">Right</option>
-              <option value="bottom">Bottom</option>
-            </select>
-          </div>
-
-          <Tooltip
-            content={this.state.content}
-            direction={this.state.direction}
-          >
-            <Button />
-          </Tooltip>
+        {/* HEADING */}
+        <h1 style={{ color: "gray", fontWeight: "bolder" }}>Tooltip</h1>
+        <div className="form-div">
+          <label style={{ color: "gray" }}>Enter Content of Tooltip</label>
+          {/* CHANGE THE TEXT OF THE TOOLTIP TEXT */}
+          <input
+            style={{ borderRadius: "10px" }}
+            type="text"
+            className="form-control"
+            onChange={handleContentInput}
+            value={content}
+          />
         </div>
+        <div className="form-div">
+          <label style={{ color: "gray" }}>Enter Direction of Tooltip</label>
+          {/* TO SELECT THE DIRECTION OF THE TOOLTIP */}
+          <select
+            className="form-select"
+            onChange={handleDirectionInput}
+            style={{ borderRadius: "10px", padding: "5px 10px" }}
+            value={direction}
+          >
+            <option value="top">Top</option>
+            <option value="left">Left</option>
+            <option value="right">Right</option>
+            <option value="bottom">Bottom</option>
+          </select>
+        </div>
+
+        <Tooltip content={content} direction={direction} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default App;
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,52 +1,34 @@
-import React, { Component } from "react";
 import "../tooltip.css";
 
-export default class Tooltip extends Component {
-  // constructor to create state for this class
-  constructor() {
-    super();
-    this.state = {
-      direction: "my-tooltiptext t-top",
-      content: "Hover Content",
-    };
-  }
+import React, { useState } from "react";
+import Button from "./Button";
 
-  //  function to change values if data change
-  hanldeTooltip = () => {
-    // setting content
-    this.setState({
-      content: this.props.content,
-    });
+const Tooltip = ({ content, direction }) => {
+  const [dir, setDir] = useState("my-tooltiptext t-top");
+  const [cont, setCont] = useState("Hover Content");
 
-    // setting up direction according to direction
-    if (this.props.direction === "top") {
-      this.setState({
-        direction: "my-tooltiptext t-top",
-      });
-    } else if (this.props.direction === "right") {
-      this.setState({
-        direction: "my-tooltiptext t-right",
-      });
-    } else if (this.props.direction === "left") {
-      this.setState({
-        direction: "my-tooltiptext t-left",
-      });
+  const hanldeTooltip = () => {
+    setCont(content);
+
+    if (direction === "top") {
+      setDir("my-tooltiptext t-top");
+    } else if (direction === "right") {
+      setDir("my-tooltiptext t-right");
+    } else if (direction === "left") {
+      setDir("my-tooltiptext t-left");
     } else {
-      this.setState({
-        direction: "my-tooltiptext t-bottom",
-      });
+      setDir("my-tooltiptext t-bottom");
     }
   };
 
-  render() {
-    return (
-      // div for bidning componet for tooltip
-      <div className="my-tooltip" onMouseEnter={this.hanldeTooltip}>
-        {/* span for tooltip text */}
-        <span className={this.state.direction}>{this.state.content}</span>
-        {/* componet fetching from where is called */}
-        {this.props.children}
-      </div>
-    );
-  }
-}
+  return (
+    // DIV FOR BINDING CLASS FOR TOOLTIP
+    <div className="my-tooltip" onMouseEnter={hanldeTooltip}>
+      {/* SPAN FOR TOOLTIP TEXT */}
+      <span className={dir}>{cont}</span>
+      <Button />
+    </div>
+  );
+};
+
+export default Tooltip;
